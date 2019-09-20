743eaffb96a0e9501b49827e565d9cd9f0195a40
Yiwen WU <yiwen26@uw.edu>
2019-09-20 19:46:54 +0800

    hw2

---
+++ 02/Program 1.py
@@ -0,0 +1,9 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+username=input("Username: ")
+print("Hello, " + username +"!")
+
---
+++ 02/Program 2.py
@@ -0,0 +1,10 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+C=float(input("Temperature in Celsius: "))
+F = C * 1.8 + 32
+print ("The temperature in Fahrenheit is "+ str(F))
+
